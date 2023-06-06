401d00b76baa3110ae58aafbf89cd692bae7ded0
syeda-sumreen-ali <sumreenali111@gmail.com>
2023-06-06 16:31:03 +0500

    react navigation in progress

--- src/route/index.js
+++ src/route/index.js
@@ -2,10 +2,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 import DrawerContent from './drawerContent';
 import Login from '../screens/login';
 import Profile from '../screens/profile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createStackNavigator();
 
-
+const Drawer = createDrawerNavigator()
 
 
 
@@ -18,11 +20,14 @@ const DrawerNavigator=()=>{
 }
 const Route=()=> {
   return (
+    <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="login" component={Login} />
       <Stack.Screen name="drawer" component={DrawerNavigator} />
      
     </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text,TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
 
 const Login = () => {
+  const navigation= useNavigation()
   return (
     <View>
       <Text>Login</Text>
+
+      <TouchableOpacity onPress={()=>navigation.navigate("drawer")}>
+      <Text>Goto profile</Text>
+      </TouchableOpacity>
+    
     </View>
   )
 }
